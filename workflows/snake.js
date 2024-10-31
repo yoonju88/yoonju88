465a4d72dc8209b5d77429d7c21ef290a6315a70
I can do it <153811235+yoonju88@users.noreply.github.com>
2024-10-31 11:10:40 +0100

    Create snake.js

---
+++ workflows/snake.js
@@ -0,0 +1,9 @@
+<div>
+  <h1 style="font-size:1em;">
+  <a href="" align="center">
+    <img src = "https://media.tenor.com/3uGzDrVIe0kAAAAi/cat-meme.gif" width =50px>
+  </a>
+  Work flows 
+</h1> 
+  <img src="https://raw.githubusercontent.com/maurodesouza/maurodesouza/output/snake.svg" alt="Snake animation" />
+</div>
